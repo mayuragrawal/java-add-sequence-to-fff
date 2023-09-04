487d03a8b1c1797cb6082fa0df70e76a0c4446c9
Mayur Agrawal <mayur.agrawal@concordusa.com>
2023-09-04 15:34:13 +0530

    started code

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+
+    public Object addSequence(Object input){
+        
+        
+        return input;
+    }
+}
